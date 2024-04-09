1680282a99343e1507e9b9f2bb486f777598ab7b
tsumi netic <diensanc@gmail.com>
2024-04-09 15:22:52 +0545

    reverting everything

--- auth.py
+++ auth.py
@@ -18,17 +18,17 @@ def login():
     #if the user fills the email and password field and clicks the login button
     if request.method=='POST':
         email=request.form.get('email')
-        password=request.form.get('password')
+        passw=request.form.get('passw')
         #checking for user details through the email entered
         user = User.query.filter_by(email=email).first()
         #if the user exists
         if user:
             #and if the password matches, login the user redirecting to the home page
-            if check_password_hash(user.password, password):
+            if check_password_hash(user.passw, passw):
                 login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
-                #flash wrong pw if passwordord doesnt match
+                #flash wrong pw if password doesnt match
                 flash('wrong pw!', category="error")
         else:
             #if the user entered doesnt exist
@@ -55,7 +55,7 @@ def signup():
     if request.method=='POST':
         email=request.form.get('email')
         name=request.form.get('name')
-        password=request.form.get('password')
+        passw=request.form.get('passw')
         #check if the user already exists
         user = User.query.filter_by(email=email).first()
         if user:
@@ -67,11 +67,11 @@ def signup():
         elif len(name)<2:
             flash('name is too short', category="error")
         #if the pw is too short 
-        elif len(password)<7:
+        elif len(passw)<7:
             flash('password is too short', category="error")
         #if everything is okay we create a instance of the model User containing the user details and hashed pw
         else:
-            new_user = User(email=email,name=name, password = generate_password_hash(password, method='pbkdf2:sha256'))
+            new_user = User(email=email,name=name, passw = generate_password_hash(passw, method='pbkdf2:sha256'))
             #add the new_user to the db and commit
             db.session.add(new_user)
             db.session.commit()
--- static/js/main.js
+++
@@ -1,7 +0,0 @@
-const menuToggle= document.querySelector(".menu-bars");
-const nav = document.querySelector("nav ul");
-
-
-menuToggle.addEventListener("click", () => {
-  nav.classList.toggle("slide");
-  });
